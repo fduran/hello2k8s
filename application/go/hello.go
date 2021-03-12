b502be20c7d1397483c5ed48950ef7ebb05b075d
fduran <duran.fernando@gmail.com>
2021-03-11 19:28:28 -0500

    Docker cache test

--- application/go/hello.go
+++ application/go/hello.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello lolz, %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hello ftw, %s!", r.URL.Path[1:])
 }
