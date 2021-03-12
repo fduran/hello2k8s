283c1096a005e2c883edee11acc6774fce4af068
fduran <duran.fernando@gmail.com>
2021-03-11 20:11:57 -0500

    Dockerfile change repo test

--- application/go/hello.go
+++ application/go/hello.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello my %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hello my dear %s!", r.URL.Path[1:])
 }
